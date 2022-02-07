2e15b432778ce4f6b77d38b0cc62d61cc201dd67
Lanie Williams <lanie_williams@baylor.edu>
2022-02-07 15:53:41 -0600

    hw

--- dictionary_exercise_part1.py
+++ dictionary_exercise_part1.py
@@ -16,12 +16,12 @@ course_time = {'CS101':'8:00 a.m.',
              'NT110':'11:00 a.m.',
              'CM241':'1:00 p.m.'}
 
-print('Please enter your course number.')
+print('Please enter your course number and press enter.')
 number = input()
 
 if number in course_room:
-    print('The room number is',course_room[number])
-    print('The instructor is',course_instr[number])
-    print('The meeting time is',course_time[number])
+    print('Room number:',course_room[number])
+    print('Instructor:',course_instr[number])
+    print('Meeting time:',course_time[number])
 else:
     print('No information available.')
